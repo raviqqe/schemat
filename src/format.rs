44777d7b24a94ae038e6affa54101415b6c9f928
Yota Toyama <raviqqe@gmail.com>
2023-07-11 23:08:01 +1000

    Fix

--- src/format.rs
+++ src/format.rs
@@ -2,7 +2,7 @@
 
 use crate::{ast::Expression, context::Context, position_map::PositionMap};
 use itertools::Itertools;
-use mfmt::{flatten, line, sequence, Document};
+use mfmt::{flatten, indent, line, r#break, sequence, Document};
 
 pub fn format(module: &[Expression], position_map: &PositionMap) -> String {
     let context = Context::new(position_map);
@@ -31,8 +31,14 @@ fn compile_expression(context: &Context, expression: &Expression) -> Document {
                 ["(".into()]
                     .into_iter()
                     .chain([compile_expressions(context, &first)])
-                    .chain(if !last.is_empty() { Some(line()) } else { None })
-                    .chain([compile_expressions(context, &last)])
+                    .chain(if last.is_empty() {
+                        None
+                    } else {
+                        Some(r#break(indent(sequence([
+                            line(),
+                            compile_expressions(context, &last),
+                        ]))))
+                    })
                     .chain([")".into()])
                     .collect::<Vec<_>>(),
             ))
@@ -58,6 +64,7 @@ fn compile_expressions(context: &Context, expressions: &[&Expression]) -> Docume
 mod tests {
     use super::*;
     use crate::{position::Position, position_map::PositionMap};
+    use indoc::indoc;
 
     #[test]
     fn format_list() {
@@ -73,6 +80,28 @@ mod tests {
                 &PositionMap::new(""),
             ),
             "(foo bar)"
+        );
+    }
+
+    #[test]
+    fn format_list_with_split_lines() {
+        assert_eq!(
+            format(
+                &[Expression::List(
+                    vec![
+                        Expression::Symbol("foo", Position::new(1, 4)),
+                        Expression::Symbol("bar", Position::new(6, 9))
+                    ],
+                    Position::new(0, 9)
+                )],
+                &PositionMap::new("(foo\nbar)"),
+            ),
+            indoc!(
+                "
+                (foo
+                  bar)
+                "
+            )
         );
     }
 
