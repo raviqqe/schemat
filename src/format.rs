c7a7535044386c81119ffd6fdc4e2d4851e02c39
Yota Toyama <raviqqe@gmail.com>
2023-07-11 16:08:54 +1000

    Fix

---
+++ src/format.rs
@@ -0,0 +1,18 @@
+use crate::ast::Expression;
+use itertools::Itertools;
+use mfmt::{line, sequence, Document};
+
+pub fn format(module: &[Expression]) -> String {
+    mfmt::format(&compile_module(module))
+}
+
+fn compile_module(module: &[Expression]) -> Document {
+    sequence(module.iter().map(compile_expression).intersperse(line()))
+}
+
+fn compile_expression(expression: &Expression) -> Document {
+    match expression {
+        Expression::Symbol(name) => name.into(),
+        _ => todo!(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod ast;
+mod format;
 mod parse;
 
 use crate::parse::parse;
