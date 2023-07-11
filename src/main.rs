25076b59d00a351807d13b6ec96f0dd88cee7984
Yota Toyama <raviqqe@gmail.com>
2023-07-11 14:39:46 +1000

    Fix

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 mod ast;
 mod parse;
 
+use crate::parse::parse;
+use std::error::Error;
+use std::process::exit;
+use std::{io::read_to_string, io::stdin};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(error) = run() {
+        eprintln!("{}", error);
+        exit(1)
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    println!("{:?}", parse(&read_to_string(stdin())?));
+
+    Ok(())
 }
--- src/parse.rs
+++ src/parse.rs
@@ -2,9 +2,8 @@ mod parser;
 
 use self::parser::{module, Span};
 use crate::ast::Expression;
+use std::error::Error;
 
-pub fn parse(source: &str) -> Result<Vec<Expression>, ()> {
-    let input = Span::new(source);
-
-    module(input).map(|(_, module)| module)
+pub fn parse(source: &str) -> Result<Vec<Expression>, Box<dyn Error + '_>> {
+    Ok(module(Span::new(source)).map(|(_, module)| module)?)
 }
