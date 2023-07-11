782f72fdbbb726d963a2862983e819dcc584af21
Yota Toyama <raviqqe@gmail.com>
2023-07-11 14:28:28 +1000

    Fix

--- src/parse/parser.rs
+++ src/parse/parser.rs
@@ -2,12 +2,11 @@ use crate::ast::Expression;
 use nom::{
     branch::alt,
     bytes::complete::take_while1,
-    character::complete::{char, multispace0, multispace1},
+    character::complete::{char, multispace0},
     combinator::{map, value},
-    error::{context, ParseError},
+    error::{context, VerboseError},
     multi::many0,
-    sequence::{delimited, preceded, separated_pair},
-    IResult,
+    sequence::{delimited, preceded},
 };
 use nom_locate::LocatedSpan;
 
@@ -15,13 +14,15 @@ const SYMBOL_SIGNS: &str = "+-*/<>=!?$%_&~^";
 
 pub type Span<'a> = LocatedSpan<&'a str>;
 
-pub fn module<'a, E: ParseError<Span<'a>>>(
-    input: Span<'a>,
-) -> IResult<Span<'a>, Vec<Expression<'a>>, E> {
+pub type Error<'a> = VerboseError<Span<'a>>;
+
+pub type IResult<'a, T> = nom::IResult<Span<'a>, T, Error<'a>>;
+
+pub fn module<'a>(input: Span<'a>) -> IResult<Vec<Expression<'a>>> {
     many0(expression)(input)
 }
 
-fn symbol<'a, E: ParseError<Span<'a>>>(input: Span<'a>) -> IResult<Span<'a>, Expression<'a>, E> {
+fn symbol<'a>(input: Span<'a>) -> IResult<Expression<'a>> {
     map(
         take_while1(|character: char| {
             character.is_alphanumeric() || SYMBOL_SIGNS.contains(character)
@@ -30,35 +31,20 @@ fn symbol<'a, E: ParseError<Span<'a>>>(input: Span<'a>) -> IResult<Span<'a>, Exp
     )(input)
 }
 
-pub fn expression<'a, E: ParseError<Span<'a>>>(
-    input: Span<'a>,
-) -> IResult<Span<'a>, Expression<'a>, E> {
+fn expression<'a>(input: Span<'a>) -> IResult<Expression<'a>> {
     alt((
         context("symbol", symbol),
         context(
             "list",
             delimited(
                 sign('('),
-                map(
-                    separated_pair(
-                        multispace0,
-                        expression,
-                        delimited(multispace1, expression, multispace0),
-                    ),
-                    |(first, rest)| {
-                        let mut items = vec![first];
-                        items.extend(rest);
-                        Expression::List(items)
-                    },
-                ),
+                map(many0(expression), Expression::List),
                 sign(')'),
             ),
         ),
     ))(input)
 }
 
-fn sign<'a, E: ParseError<Span<'a>>>(
-    character: char,
-) -> impl Fn(Span<'a>) -> IResult<Span<'a>, (), E> {
+fn sign<'a>(character: char) -> impl Fn(Span<'a>) -> IResult<()> {
     move |input| value((), preceded(multispace0, char(character)))(input)
 }
