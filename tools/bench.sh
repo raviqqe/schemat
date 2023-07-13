bb738973b4adec397dfc77aa8d967b89b2f84b45
Yota Toyama <raviqqe@gmail.com>
2023-07-13 20:16:23 +1000

    Fix

--- tools/bench.sh
+++ tools/bench.sh
@@ -3,8 +3,9 @@
 set -e
 
 cargo install hyperfine
+cargo build --release
 
 mkdir -p tmp
 $(dirname $0)/generate_s_expressions.sh 5 >tmp/foo.scm
 
-hyperfine 'schemat <tmp/foo.scm >/dev/null'
+hyperfine 'target/release/schemat <tmp/foo.scm >/dev/null'
