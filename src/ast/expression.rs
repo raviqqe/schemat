57b791c6e3697e236bd39d9da562b37eb393310e
Yota Toyama <raviqqe@gmail.com>
2023-07-13 23:08:50 +1000

    Fix

--- src/ast/expression.rs
+++ src/ast/expression.rs
@@ -1,7 +1,7 @@
 use crate::position::Position;
 use std::alloc::Allocator;
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug)]
 pub enum Expression<'a, A: Allocator> {
     List(Vec<Expression<'a, A>, A>, Position),
     Quote(Box<Expression<'a, A>, A>, Position),
@@ -17,5 +17,42 @@ impl<'a, A: Allocator> Expression<'a, A> {
             Self::String(_, position) => position,
             Self::Symbol(_, position) => position,
         }
+    }
+}
+
+// TODO Why do we need to do this manually?
+impl<'a, A: Allocator> PartialEq for Expression<'a, A> {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::List(one, position), Self::List(other, other_position)) => {
+                one == other && position == other_position
+            }
+            (Self::Quote(one, position), Self::Quote(other, other_position)) => {
+                one == other && position == other_position
+            }
+            (Self::String(one, position), Self::String(other, other_position)) => {
+                one == other && position == other_position
+            }
+            (Self::Symbol(one, position), Self::Symbol(other, other_position)) => {
+                one == other && position == other_position
+            }
+            _ => false,
+        }
+    }
+}
+
+impl<'a, A: Allocator> Eq for Expression<'a, A> {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::alloc::Global;
+
+    #[test]
+    fn equal() {
+        assert_eq!(
+            Expression::<Global>::Symbol("foo", Position::new(0, 0)),
+            Expression::<Global>::Symbol("foo", Position::new(0, 0))
+        );
     }
 }
