12f6c3368537b181e4ecd804c10897550404810e
Yota Toyama <raviqqe@gmail.com>
2023-07-13 21:41:55 +1000

    Fix

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,13 @@ mod parse;
 mod position;
 mod position_map;
 
+use bumpalo::Bump;
 use crate::{
     format::format,
     parse::{parse, parse_comments, parse_hash_directives, ParseError},
     position_map::PositionMap,
 };
 use std::{
-    alloc::Global,
     error::Error,
     io::{read_to_string, stdin},
     process::exit,
@@ -30,13 +30,14 @@ fn run() -> Result<(), Box<dyn Error>> {
     let source = read_to_string(stdin())?;
     let position_map = PositionMap::new(&source);
     let convert_error = |error: ParseError| error.to_string(&source, &position_map);
+    let allocator= Bump::new();
 
     print!(
         "{}",
         format(
-            &parse::<Global>(&source, Global).map_err(convert_error)?,
-            &parse_comments::<Global>(&source, Global).map_err(convert_error)?,
-            &parse_hash_directives::<Global>(&source, Global).map_err(convert_error)?,
+            &parse(&source, &allocator).map_err(convert_error)?,
+            &parse_comments(&source, &allocator).map_err(convert_error)?,
+            &parse_hash_directives(&source, &allocator).map_err(convert_error)?,
             &position_map,
         )
     );
