4daed4e1bd35de844fd8333e359f3c08c42d36c3
Yota Toyama <raviqqe@gmail.com>
2023-07-21 21:09:25 +1000

    Fix

--- src/parse/parser.rs
+++ src/parse/parser.rs
@@ -6,7 +6,7 @@ use crate::{
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_until, take_while, take_while1},
-    character::complete::{char, multispace0, multispace1, none_of, satisfy, space0},
+    character::complete::{anychar, char, multispace0, multispace1, none_of, satisfy, space0},
     combinator::{all_consuming, cut, map, recognize, value},
     error::context,
     multi::{fold_many0, many0_count, many1_count},
@@ -59,6 +59,11 @@ fn symbol<A: Allocator + Clone>(input: Input<A>) -> IResult<Expression<A>, A> {
         token(positioned(alt((
             recognize(tuple((
                 satisfy(is_head_symbol_character),
+                take_while(is_tail_symbol_character),
+            ))),
+            recognize(tuple((
+                tag("#\\"),
+                anychar,
                 take_while(is_tail_symbol_character),
             ))),
             recognize(tuple((
