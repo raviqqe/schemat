67150732bde0ef959ec139451baa5caa0b542883
Yota Toyama <raviqqe@gmail.com>
2024-01-19 21:14:27 +0900

    Fix

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,22 @@ use crate::{
 };
 use bumpalo::Bump;
 use clap::Parser;
+use futures::future::try_join_all;
 use std::{
+    collections::BTreeSet,
     error::Error,
     io::{read_to_string, stdin},
     path::PathBuf,
     process::exit,
 };
+use tokio::task::spawn_blocking;
 
 #[derive(clap::Parser)]
 #[command(about, version)]
 struct Arguments {
     /// Paths of files or directories to format or check the format of.
     #[arg()]
-    paths: Vec<PathBuf>,
+    paths: Vec<String>,
     /// Check if files are formatted correctly.
     #[arg(short, long)]
     check: bool,
@@ -34,13 +37,38 @@ struct Arguments {
 
 #[tokio::main]
 async fn main() {
-    if let Err(error) = run(Arguments::parse()) {
+    if let Err(error) = run(Arguments::parse()).await {
         eprintln!("{}", error);
         exit(1)
     }
 }
 
-fn run(_arguments: Arguments) -> Result<(), Box<dyn Error>> {
+async fn run(arguments: Arguments) -> Result<(), Box<dyn Error>> {
+    if arguments.paths.is_empty() {
+        return format_stdin();
+    }
+
+    let paths = try_join_all(
+        arguments
+            .paths
+            .into_iter()
+            .map(|path| spawn_blocking(move || glob::glob(&path))),
+    )
+    .await?
+    .into_iter()
+    .collect::<Result<Vec<_>, _>>()?
+    .into_iter()
+    .flatten()
+    .collect::<Result<BTreeSet<_>, _>>()?;
+
+    if arguments.check {
+        check(&paths)
+    } else {
+        format_paths(&paths)
+    }
+}
+
+fn format_stdin() -> Result<(), Box<dyn Error>> {
     let source = read_to_string(stdin())?;
     let position_map = PositionMap::new(&source);
     let convert_error = |error: ParseError| error.to_string(&source, &position_map);
@@ -58,4 +86,12 @@ fn run(_arguments: Arguments) -> Result<(), Box<dyn Error>> {
     );
 
     Ok(())
+}
+
+fn check(paths: &BTreeSet<PathBuf>) -> Result<(), Box<dyn Error>> {
+    todo!()
+}
+
+fn format_paths(paths: &BTreeSet<PathBuf>) -> Result<(), Box<dyn Error>> {
+    todo!()
 }
