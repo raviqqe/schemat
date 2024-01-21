c1c78a4d69e4c2be11bbbe9d4a64aee388be7764
Yota Toyama <raviqqe@gmail.com>
2024-01-21 14:54:18 +0900

    Fix

--- src/parse/parser.rs
+++ src/parse/parser.rs
@@ -816,7 +816,7 @@ mod tests {
             fn parse_one_line() {
                 assert_eq!(
                     comments(Input::new_extra("#|foo|#", Global)).unwrap().1,
-                    vec![]
+                    vec![Comment::new("#|foo|#", Position::new(0, 10))]
                 );
             }
 
