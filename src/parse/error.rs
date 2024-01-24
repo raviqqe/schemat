d93fc0fc77e7a903da8e2822f1c2fe1995868449
Yota Toyama <raviqqe@gmail.com>
2024-01-24 18:47:52 +0900

    Fix

--- src/parse/error.rs
+++ src/parse/error.rs
@@ -48,7 +48,6 @@ impl ParseError {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use indoc::indoc;
     use nom::error::ErrorKind;
     use pretty_assertions::assert_eq;
     use std::alloc::Global;
@@ -68,13 +67,7 @@ mod tests {
 
         assert_eq!(
             error.to_string("foo.scm", source, &position_map),
-            indoc!(
-                "
-                    failed to parse
-                    foo.scm:1:1: foo
-                "
-            )
-            .trim()
+            "failed to parse foo.scm:1:1: foo"
         );
     }
 }
