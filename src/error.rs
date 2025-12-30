bdb5ed6bf681b7bd107edfe10cb248f3e78cb6e7
Yota Toyama <raviqqe@gmail.com>
2025-12-30 08:56:59 -0800

    Revert "Fix"
    This reverts commit 0cf016fddb36a5b425a1a79a599606a29cb6a1b8.

--- src/error.rs
+++ src/error.rs
@@ -3,17 +3,14 @@ use core::{
     fmt,
     fmt::{Display, Formatter},
 };
-use glob::{GlobError, PatternError};
 use std::io;
 
 #[derive(Debug)]
 pub enum ApplicationError {
     Format(fmt::Error),
-    Glob(GlobError),
-    Ignore(ignore::Error),
+    Glob(ignore::Error),
     Io(io::Error),
     Parse(String),
-    Pattern(PatternError),
 }
 
 impl Error for ApplicationError {}
@@ -23,10 +20,8 @@ impl Display for ApplicationError {
         match self {
             Self::Format(error) => error.fmt(formatter),
             Self::Glob(error) => error.fmt(formatter),
-            Self::Ignore(error) => error.fmt(formatter),
             Self::Io(error) => error.fmt(formatter),
             Self::Parse(error) => error.fmt(formatter),
-            Self::Pattern(error) => error.fmt(formatter),
         }
     }
 }
@@ -37,26 +32,14 @@ impl From<fmt::Error> for ApplicationError {
     }
 }
 
-impl From<GlobError> for ApplicationError {
-    fn from(error: GlobError) -> Self {
-        Self::Glob(error)
-    }
-}
-
 impl From<ignore::Error> for ApplicationError {
     fn from(error: ignore::Error) -> Self {
-        Self::Ignore(error)
+        Self::Glob(error)
     }
 }
 
 impl From<io::Error> for ApplicationError {
     fn from(error: io::Error) -> Self {
         Self::Io(error)
-    }
-}
-
-impl From<PatternError> for ApplicationError {
-    fn from(error: PatternError) -> Self {
-        Self::Pattern(error)
     }
 }
